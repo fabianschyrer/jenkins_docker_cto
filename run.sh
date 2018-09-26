61e0a317ac93ef7fda6a8116cf17da9a4b02644d
fabian <fabian@fabians-MacBook-Pro.local>
2018-09-26 10:39:06 +0700

    added docker network

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
-# Ruin Jenkins Docker Container
+# Create Docker network
+docker network create --driver bridge --subnet 172.0.0.0/29 jenkins-network &>/dev/null || :
+
+# Run Jenkins Docker Container
 docker run -d \
-	--name Jenkins-CTO \
+	--name jenkins-cto \
 	--volume jenkins_home:/var/jenkins_home \
 	--publish 8080:8080 \
 	--publish 50000:50000 \
 	jenkins.docker.cto:latest
 
-docker logs -f Jenkins-CTO
+docker logs -f jenkins-cto
 
 
 
