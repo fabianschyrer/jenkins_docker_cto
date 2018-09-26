c228df65d2ef807a0cf0baa04d935582721a4fd0
fabian <fabian@fabians-MacBook-Pro.local>
2018-09-26 12:46:35 +0700

    clean

--- run.sh
+++ run.sh
@@ -7,10 +7,6 @@ docker run -d \
 	--volume jenkins_home:/var/jenkins_home \
 	--network="jenkins-network" \
 	--publish 8080:8080 \
-	--publish 50000:50000 \
 	jenkins.docker.cto:latest
 
 # docker logs -f jenkins-cto
-
-
-
