77c56ed91857dad2f09333eaec0c74a2862a3b50
Fabian Alexander Schyrer <fabian@central.tech>
2018-09-27 17:14:20 +0700

    added right repository to push

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Build & Tag Jenkins Image
 docker build -t jenkins.docker.cto:latest .
-#docker tag jenkins.docker.cto:latest <DOCKER_REGISTRY>/jenkins.docker.cto:latest
+docker tag jenkins.docker.cto:latest fabianschyrer/jenkins.docker.cto:latest
